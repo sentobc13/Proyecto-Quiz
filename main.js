478ae41b1cd58fa70eecf47ec96d94c02ea10998
sentobc13 <sentoberlanga@gmail.com>
2024-04-12 13:50:28 +0200

    Proyecto Quiz api traido

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+
+
 const API_URL = "https://quizapi.io/api/v1/questions?apiKey=j3qCjpMiS3nj5Hj1aqOXiDmaL7LORKnPePdbzFWs&limit=10"
+
+axios.get(API_URL)
+.then((res)=>console.log(res))
+.catch((err)=>console.error(err))
+
+
+const hideViews = ()=> {
+    startButton.classlist.add("hide")
+}
