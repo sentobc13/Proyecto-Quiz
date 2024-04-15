f315b3d86e5973fe7ba9ebbc46f2278d5b4c1a2b
sentobc13 <sentoberlanga@gmail.com>
2024-04-15 13:42:02 +0200

    Proyecto Quiz bootswatch, intentando getQuestions y showQuestions

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
+const questionElement = document.getElementById("questions");
 const answerButtonsElement = document.getElementById("answer-buttons");
 
+let currentQuestion = 0;
+let score = 0;
+let questions = [];
 
+const API_URL =
+  "https://quizapi.io/api/v1/questions?apiKey=j3qCjpMiS3nj5Hj1aqOXiDmaL7LORKnPePdbzFWs&limit=10";
 
-const API_URL = "https://quizapi.io/api/v1/questions?apiKey=j3qCjpMiS3nj5Hj1aqOXiDmaL7LORKnPePdbzFWs&limit=10"
+const getQuestions = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    questions = response.data;
+    showQuestions();
+  } catch (error) {
+    console.error(error);
+  }
+};
+getQuestions();
 
-axios.get(API_URL)
-.then((res)=>console.log(res))
-.catch((err)=>console.error(err))
+const showQuestions = async () => {
+  try {
+    const question = questions[currentQuestion];
+    questionElement.innerHTML = question.question
+    console.log(question);
+  } catch (error) {}
+};
 
-
-const hideViews = ()=> {
-    startButton.classlist.add("hide")
-}
+startButton.addEventListener('click', getQuestions);
