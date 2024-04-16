7f8d11b320201362a255a952be87f0a64bd7ff06
sentobc13 <sentoberlanga@gmail.com>
2024-04-16 11:33:33 +0200

    Proyecto Quiz seguimos con showQuestions

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+const quizNavbar = document.getElementById("quiz-navbar");
+const homeDiv = document.getElementById("home")
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
-const questionContainerElement = document.getElementById("question-container");
-const questionElement = document.getElementById("questions");
-const answerButtonsElement = document.getElementById("answer-buttons");
+const questionsAPI = document.getElementById("questions");
+const answerCards = document.getElementById("answer-cards");
 
 let currentQuestion = 0;
 let score = 0;
@@ -11,23 +12,64 @@ let questions = [];
 const API_URL =
   "https://quizapi.io/api/v1/questions?apiKey=j3qCjpMiS3nj5Hj1aqOXiDmaL7LORKnPePdbzFWs&limit=10";
 
-const getQuestions = async () => {
+const getInfo = async () => {
   try {
     const response = await axios.get(API_URL);
     questions = response.data;
-    showQuestions();
   } catch (error) {
     console.error(error);
   }
 };
-getQuestions();
+getInfo();
 
-const showQuestions = async () => {
+
+const startQuiz = async (e) => {
+ e.preventDefault()
   try {
+    homeDiv.classList.add("d-none")
     const question = questions[currentQuestion];
-    questionElement.innerHTML = question.question
-    console.log(question);
+    questionsAPI.innerText = question.question;
+    //pintar las respuestas en botones
+    console.log(question.answers);
+    Object.values(question.answers).forEach(answer => {
+      console.log(answer)
+    
+    })
+    // console.log(question);
   } catch (error) {}
 };
 
-startButton.addEventListener('click', getQuestions);
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+
+    if (answer.correct) {
+      button.dataset.correct = true;
+    }
+
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+// questions.forEach((question) => {
+//     const questionsAPI = document.createElement("questions");
+//     questionsAPI.classList.add("questions");
+//     questionsAPI.textContent = question.question;
+//     const answersElement = document.createElement("ul");
+  
+//     answersElement.classList.add("answers");
+  
+//     question.answers.forEach((answer) => {
+//       const answerItem = document.createElement("li");
+//       answerItem.textContent = answer.answer;
+//       answersElement.appendChild(answerItem);
+  
+//       quizContainer.appendChild(questionElement);
+//       quizContainer.appendChild(answersElement);
+//     });
+//   });
+
+  
+startButton.addEventListener("click", startQuiz);
