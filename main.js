09b7a960cd44326d8852fccbb284b087d6177b58
sentobc13 <sentoberlanga@gmail.com>
2024-04-17 15:25:59 +0200

    Proyecto Quiz pinta las respuestas y las desabilita

--- main.js
+++ main.js
@@ -22,58 +22,74 @@ const getInfo = async () => {
 };
 getInfo();
 
-const startQuiz = async (e) => {
-  e.preventDefault();
-  try {
-    homeDiv.classList.add("d-none");
-    const question = questions[currentQuestion];
-    questionsAPI.innerText = question.question;
-    let correctAnswer = "";
-
-    Object.keys(question.correct_answers).forEach((answer) => {
-      if (question.correct_answers[answer] == "true") {
-        correctAnswer = answer.substring(0, 8);
-      }
-    });
-
-    Object.values(question.answers).forEach((answer) => {
-      if (answer !== null) {
-        const button = document.createElement("button");
-        button.innerText = answer;
-        answerCards.appendChild(button);
-        console.log("answer", answer);
-        console.log("correct answer", question.answers[correctAnswer]);
-
-        if (answer === question.answers[correctAnswer]) {
-          button.dataset.correctAnswer = true;
-        } else {
-        button.dataset.correctAnswer = false;
-        }
-        button.addEventListener("click",function() {
-          setStatusClass(button, button.dataset.correctAnswer === "true");
-          if (button.dataset.correctAnswer === "true") {
-            score++;
-          }
-        })
-      }
-    });
-    questionsAPI.appendChild(answerCards);
-  } catch (error) {
-    console.error(error);
+const backGroundColorAns = (button) => {
+  button.setAttribute("disabled", "");
+  if (button.dataset.correctAnswer == "true") {
+    button.classList.add("correct");
+  } else {
+    button.classList.add("wrong");
   }
+};
+nextButton.style.display = 'block';
+
+const selectAnswer = (answerSelected) => {
+  Array.from(answerCards.children).forEach((button) => {
+    backGroundColorAns(button);
+  });
+};
+
+const startQuiz = (e) => {
+  e.preventDefault();
+
+  homeDiv.classList.add("d-none");
+  const question = questions[currentQuestion];
+  console.log(question);
+  questionsAPI.innerText = question.question;
+  let correctAnswer = "";
+  Object.keys(question.correct_answers).forEach((answer) => {
+    if (question.correct_answers[answer] == "true") {
+      correctAnswer = answer.substring(0, 8);
+    }
+  });
+
+  Object.values(question.answers).forEach((answer) => {
+    if (answer !== null) {
+      const button = document.createElement("button");
+      button.innerText = answer;
+      answerCards.appendChild(button);
+      console.log("answer", answer);
+      console.log("correct answer", question.answers[correctAnswer]);
+
+      if (answer === question.answers[correctAnswer]) {
+        button.dataset.correctAnswer = true;
+        score++;
+      } else {
+        button.dataset.correctAnswer = false;
+      }
+      button.addEventListener("click", function () {
+        selectAnswer(button)
+        // setStatusClass(button, button.dataset.correctAnswer === "true");
+       
+      });
+    }
+  });
+  questionsAPI.appendChild(answerCards);
+
   function setStatusClass(element, isCorrect) {
     element.classList.add(isCorrect ? "correct" : "wrong");
   }
 };
 
-const showNextQuestion = () => {
-  currentQuestion++;
-  if (currentQuestion < questions.length) {
-    const question = questions[currentQuestion];
-    questionsAPI.innerText = question.question;
-    // Limpia las respuestas anteriores
-    while (answerCards.firstChild) {
-      answerCards.removeChild(answerCards.firstChild);
-    }}}
+// const showNextQuestion = () => {
+//   currentQuestion++;
+//   if (currentQuestion < questions.length) {
+//     const question = questions[currentQuestion];
+//     questionsAPI.innerText = question.question;
+//     // Limpia las respuestas anteriores
+//     while (answerCards.firstChild) {
+//       answerCards.removeChild(answerCards.firstChild);
+//     }
+//   }
+// };
 
 startButton.addEventListener("click", startQuiz);
